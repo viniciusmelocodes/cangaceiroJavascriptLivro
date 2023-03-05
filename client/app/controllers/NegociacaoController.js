190462f014443d02af7be9f0d431bc566a8c54d1
Vinícius Melo <viniciusldemelo@gmail.com>
2023-03-05 00:49:35 -0300

    Novo projeto CangaceiroJavascript

---
+++ client/app/controllers/NegociacaoController.js
@@ -0,0 +1,36 @@
+class NegociacaoController {
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+        this._negociacoes = new Negociacoes();
+        this._negociacoesView = new NegociacoesView('#negociacoes');
+
+        this._negociacoesView.update(this._negociacoes);
+    }
+
+    adiciona(event) {
+        event.preventDefault();
+
+        this._negociacoes.adiciona(this._criaNegociacao());
+        this._negociacoesView.update(this._negociacoes);
+        this._limpaFormulario();
+    }
+
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+        this._inputData.focus();
+    }
+
+    _criaNegociacao() {
+        // retorna uma instância de negociação
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value)
+        );
+    }
+}
